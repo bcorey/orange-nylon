a4179eee45bd5755b6f53bed1236172d20530029
Benjamin Corey <benjaminmcorey@gmail.com>
2023-04-18 01:40:49 -0400

    new manifest

---
+++ posts/2023-04-10-2/content.md
@@ -0,0 +1,3 @@
+Hi, my name is Benjamin! I'm a Unity-certified developer who most recently interned with Onshape at PTC, coding CAD modeling tools for the web. I'm currently finishing one last metaphorical bowl of computer science + art & design soup at James Madison University.
+
+This site is currently under construction but thanks for stopping by!
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
-use std::fs;
+use std::{fs};
 use std::fs::DirEntry;
 use std::io::Write;
 
 #[derive(serde::Deserialize, serde::Serialize)]
 struct Manifest {
-    pinned_projects: Vec<String>,
-    pinned_posts: Vec<String>,
-    recent_posts: Vec<String>,
+    bio: Vec<PostMetadata>,
+    pinned_projects: Vec<PostMetadata>,
+    pinned_posts: Vec<PostMetadata>,
+    recent_posts: Vec<PostMetadata>,
     all: Vec<String>
+}
+
+impl Manifest {
+    pub fn new() -> Self {
+        Manifest {
+            bio: Vec::new(),
+            pinned_projects: Vec::new(),
+            pinned_posts: Vec::new(),
+            recent_posts: Vec::new(),
+            all: Vec::new(),
+        }
+    }
 }
 
 fn main() {
     let mut post_list: Vec<PostEntry> = Vec::new();
-
 
     let posts_dir = fs::read_dir("./posts").unwrap();
 
@@ -32,44 +44,80 @@ fn main() {
         println!("{}", entry.dir_name);
     }
 
-    let manifest_string = fs::read_to_string(
-        "posts/manifest.yaml".to_string()).unwrap();
-    let mut manifest_content = serde_yaml::from_str::<Manifest>(&manifest_string).unwrap();
-    let all: Vec<String> = post_list
-        .iter()
-        .filter(|entry| !entry.is_ok())
-        .map(|post_entry| post_entry.dir_name.clone())
+    let mut manifest_content = Manifest::new();
+
+    let bio: Vec<PostMetadata> = post_list.iter().filter(|entry| entry.is_ok())
+        .map(|post_entry| post_entry.meta_value.as_ref().unwrap().clone())
+        .filter(|post_meta| post_meta.content_type == ContentType::Note && post_meta.tags.contains(&"bio".to_string()))
+        .collect();
+    manifest_content.bio = bio;
+
+    let pinned_projects: Vec<PostMetadata> = post_list.iter().filter(|entry| entry.is_ok())
+        .map(|post_entry| post_entry.meta_value.as_ref().unwrap().clone())
+        .filter(|post_meta| post_meta.is_pinned && post_meta.content_type == ContentType::Project)
+        .collect();
+    manifest_content.pinned_projects = pinned_projects;
+
+    let pinned_posts: Vec<PostMetadata> = post_list.iter().filter(|entry| entry.is_ok())
+        .map(|post_entry| post_entry.meta_value.as_ref().unwrap().clone())
+        .filter(|post_meta| post_meta.is_pinned && post_meta.content_type == ContentType::Post)
+        .collect();
+    manifest_content.pinned_posts = pinned_posts;
+
+    let all: Vec<String> = post_list.iter().filter(|entry| entry.is_ok())
+        .map(|post_entry| {
+            post_entry.dir_name.clone().replace("./posts/", "")
+        })
         .collect();
     manifest_content.all = all;
     let manifest_write = serde_yaml::to_string::<Manifest>(&manifest_content).unwrap();
-
-    fs::write("posts/manifest.yaml", manifest_write).expect("Could not write manifest");
+    fs::write("./posts/manifest.yaml", manifest_write).expect("Could not write manifest");
 }
 
 pub struct PostEntry {
     pub dir_name: String,
     pub content: bool,
     pub ok_meta: bool,
+    pub meta_value: Option<PostMetadata>
 }
 
 impl PostEntry {
     pub fn new(dir_name: String) -> Self {
-        PostEntry { dir_name: dir_name, content: false, ok_meta: false }
+        PostEntry { dir_name: dir_name, content: false, ok_meta: false, meta_value: None }
     }
 
     pub fn is_ok(&self) -> bool {
-        self.content && self.ok_meta
+        self.content && self.ok_meta && self.meta_value.is_some()
     }
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Clone, Copy, PartialEq, serde::Serialize, serde::Deserialize)]
+pub enum ContentType {
+    Post,
+    Project,
+    Note
+}
+use std::fmt;
+impl fmt::Display for ContentType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ContentType::Post => write!(f, "Post"),
+            ContentType::Project => write!(f, "Project"),
+            ContentType::Note => write!(f, "Note"),
+        }
+    }
+}
+
+
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
 pub struct PostMetadata {
     pub title: String,
     pub tagline: String,
     pub date: String,
     pub tags: Vec<String>,
     pub thumbnails: Vec<String>,
-    pub splash_image: String,
+    pub is_pinned: bool,
+    pub content_type: ContentType,
 }
 
 fn loop_projects(post_entry: DirEntry) -> PostEntry {
@@ -96,7 +144,13 @@ fn loop_projects(post_entry: DirEntry) -> PostEntry {
             } else if file.file_name() == "meta.yaml" {
                 let meta_content = fs::read_to_string(file.path().into_os_string()).unwrap();
                 match serde_yaml::from_str::<PostMetadata>(&meta_content) {
-                    Ok(_) => entry.ok_meta = true,
+                    Ok(meta_value) => {
+                        entry.ok_meta = true;
+                        let mut_meta_value = meta_value.clone();
+                        //mut_meta_value.thumbnails = add_server_prefix_thumbnails(meta_value.thumbnails, entry.dir_name.clone());
+                        entry.meta_value = Some(mut_meta_value);
+                        
+                    },
                     Err(e) => println!("Error reading meta.yaml at {}\nerror: {}", entry.dir_name, e),
                 }
             }
@@ -133,6 +187,16 @@ fn add_server_prefix(html_string: String, dir_name: String) -> String {
     }
 
     html_final
+}
+
+fn _add_server_prefix_thumbnails(thumbnails: Vec<String>, dir_name: String) -> Vec<String> {
+    let path_prefix = format!("{}/{}/", IMG_SERVER_PREFIX, dir_name);
+    let mut new_paths: Vec<String> = Vec::new();
+    for image in thumbnails {
+        new_paths.push(format!("{}{}", path_prefix, image));
+    }
+
+    new_paths
 }
 
 // we need a terminal app that writes the metadata based on the content of each directory in /posts
